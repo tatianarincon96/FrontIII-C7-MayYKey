125f28c598993374b5e44869c30b5f2ae3add8f0
tatianarincon96 <tatianalunarincon@gmail.com>
2021-08-23 16:31:22 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    const words = ["HOLA","HOLA","HOLA","MUNDO"];
+
+    return (
+    <div>
+      <h1>HOLA</h1>
+      <h1>HOLA</h1>
+      <h1>HOLA</h1>
+      <h1>MUNDO</h1>
+    </div>
+    )
+  }
+}
+
+export default App;
